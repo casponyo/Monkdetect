c11c0be8a88b82e6bdc3793cbd3e4bcc04e7383f
casponyo <157831169+casponyo@users.noreply.github.com>
2024-09-16 23:19:38 +0300

    create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -12,14 +12,24 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
+            tmpi = int(member [5] [0].text) 
+            tmps = member [5] [0].text 
+            if tmpi > int (member [5] [2].text): 
+                member [5] [0].text = member [5] [2].text
+                member [5] [2].text = tmps 
+            tmpi = int (member [5] [1].text) 
+            tmps = member [5] [1].text 
+            if tmpi > int (member [5] [3].text):
+                member [5] [1].text = member [5] [3].text 
+                member [5] [3].text = tmps
             value = (root.find('filename').text,
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
+                     int(member[5][0].text),
+                     int(member[5][1].text),
+                     int(member[5][2].text),
+                     int(member[5][3].text)
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
